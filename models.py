034ef26c496fc05b529c34ed59fea9b23fb15974
vortex-ninja <vortex.ninja@protonmail.com>
2018-05-09 12:39:28 +0200

    Add model for Note objects

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+import os
+
+
+engine = create_engine(os.environ["DATABASE_URL"], echo=True)
+Base = declarative_base()
+
+
+class Note(Base):
+    __tablename__ = 'notes'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    version = Column(Integer, primary_key=True, default=1)
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    modified = Column(DateTime(timezone=True), server_default=func.now())
+    deleted = Column(Boolean, default=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
